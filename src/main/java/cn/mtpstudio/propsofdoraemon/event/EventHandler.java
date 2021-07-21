643e25f647e2456e97eaaa8f8dde1b6e4590bc3e
Frank__Wang <frank2007shandong@outlook.com>
2021-07-21 14:30:51 +0800

    优化EventHandler中if语句

--- src/main/java/cn/mtpstudio/propsofdoraemon/event/EventHandler.java
+++ src/main/java/cn/mtpstudio/propsofdoraemon/event/EventHandler.java
@@ -6,7 +6,6 @@ import cn.mtpstudio.propsofdoraemon.item.ItemBambooCopter;
 import net.minecraft.entity.monster.ZombieEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
-import net.minecraft.potion.Effect;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
 import net.minecraftforge.event.entity.living.PotionEvent;
@@ -21,45 +20,45 @@ public class EventHandler {
 
     @SubscribeEvent
     public static void onLivingEquipmentChange(LivingEquipmentChangeEvent e) {
-        if (!(e.getEntityLiving() instanceof PlayerEntity && e.getSlot().equals(EquipmentSlotType.HEAD) && (e.getFrom().getItem() instanceof ItemBambooCopter || e.getTo().getItem() instanceof ItemBambooCopter)))
-            return;
-        PlayerEntity player = (PlayerEntity) e.getEntityLiving();
-        if (e.getTo().getItem() instanceof ItemBambooCopter) {
-            player.abilities.allowFlying = true;
-        } else {
-            if (!player.isCreative()) {
-                player.abilities.allowFlying = false;
-                player.abilities.isFlying = false;
+        if ((e.getEntityLiving() instanceof PlayerEntity && e.getSlot().equals(EquipmentSlotType.HEAD) && (e.getFrom().getItem() instanceof ItemBambooCopter || e.getTo().getItem() instanceof ItemBambooCopter))) {
+            PlayerEntity player = (PlayerEntity) e.getEntityLiving();
+            if (e.getTo().getItem() instanceof ItemBambooCopter) {
+                player.abilities.allowFlying = true;
+            } else {
+                if (!player.isCreative()) {
+                    player.abilities.allowFlying = false;
+                    player.abilities.isFlying = false;
+                }
             }
+            player.sendPlayerAbilities();
         }
-        player.sendPlayerAbilities();
     }
 
     @SubscribeEvent
     public static void onPotionAdded(PotionEvent.PotionAddedEvent e) {
-        if (!(e.getEntityLiving() instanceof PlayerEntity && e.getPotionEffect().getPotion() instanceof EffectAdventure))
-            return;
-        PlayerEntity player = (PlayerEntity) e.getEntityLiving();
-        Thread thread = new Thread(() -> {
-            player.removePotionEffect(Effects.EFFECT_ADVENTURE);
-            World world = player.getEntityWorld();
-            List<Double> x = new ArrayList<>(), y = new ArrayList<>(), z = new ArrayList<>();
-            for (int i = 0; i < 10; i++) {
-                x.add(player.getPosXRandom(10));
-                y.add(player.getPosYRandom());
-                z.add(player.getPosZRandom(10));
-            }
-            try {
-                Thread.sleep(10000L);
-            } catch (InterruptedException err) {
-                err.printStackTrace();
-            }
-            for (int i = 1; i < 11; i++) {
-                ZombieEntity zombieChan = new ZombieEntity(world);
-                zombieChan.setPosition(x.get(i), y.get(i), z.get(i));
-                world.addEntity(zombieChan);
-            }
-        });
-        thread.start();
+        if ((e.getEntityLiving() instanceof PlayerEntity && e.getPotionEffect().getPotion() instanceof EffectAdventure)) {
+            PlayerEntity player = (PlayerEntity) e.getEntityLiving();
+            Thread thread = new Thread(() -> {
+                player.removePotionEffect(Effects.EFFECT_ADVENTURE);
+                World world = player.getEntityWorld();
+                List<Double> x = new ArrayList<>(), y = new ArrayList<>(), z = new ArrayList<>();
+                for (int i = 0; i < 10; i++) {
+                    x.add(player.getPosXRandom(10));
+                    y.add(player.getPosYRandom());
+                    z.add(player.getPosZRandom(10));
+                }
+                try {
+                    Thread.sleep(10000L);
+                } catch (InterruptedException err) {
+                    err.printStackTrace();
+                }
+                for (int i = 1; i < 11; i++) {
+                    ZombieEntity zombieChan = new ZombieEntity(world);
+                    zombieChan.setPosition(x.get(i), y.get(i), z.get(i));
+                    world.addEntity(zombieChan);
+                }
+            });
+            thread.start();
+        }
     }
 }
