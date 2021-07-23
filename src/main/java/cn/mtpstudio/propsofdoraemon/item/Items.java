ffc939b2a7dddb277f3a0c0b265b0543b361d064
Frank__Wang <frank2007shandong@outlook.com>
2021-07-23 19:09:38 +0800

    重构Items

--- src/main/java/cn/mtpstudio/propsofdoraemon/item/Items.java
+++ src/main/java/cn/mtpstudio/propsofdoraemon/item/Items.java
@@ -4,14 +4,25 @@ import net.minecraft.item.Item;
 
 import java.util.ArrayList;
 
+/**
+ * 注册物品
+ * @author Frank__Wang ShootKing
+ */
+
 public class Items {
     public static final ArrayList<Item> MOD_ITEMS = new ArrayList<>();
     public static final ItemBambooCopter BAMBOO_COPTER = new ItemBambooCopter();
-    public static final ItemTeapot TEAPOT = new ItemTeapot();
+    public static final ItemEmptyTeapot EMPTY_TEAPOT = new ItemEmptyTeapot();
+    public static final ItemAdventureTea ADVENTURE_TEA = new ItemAdventureTea();
     public static final ItemHelpBall HELP_BALL = new ItemHelpBall();
     static {
+        // 添加mod物品竹蜻蜓
         MOD_ITEMS.add(BAMBOO_COPTER);
-        MOD_ITEMS.add(TEAPOT);
+        // 添加mod物品空茶杯
+        MOD_ITEMS.add(EMPTY_TEAPOT);
+        // 添加mod物品冒险茶
+        MOD_ITEMS.add(ADVENTURE_TEA);
+        // 添加mod物品救命丸子
         MOD_ITEMS.add(HELP_BALL);
     }
 }
