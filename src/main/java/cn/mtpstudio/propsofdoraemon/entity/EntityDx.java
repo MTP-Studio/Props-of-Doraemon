f6086d160ce6c71a6ba88ff2555b3e1314f4896e
JayMan <59107518+BitbanChou@users.noreply.github.com>
2021-07-27 20:40:58 +0800

    4 guys
    Da Xiong、Jing Xiang、Pang Hu、Xiao Fu

---
+++ src/main/java/cn/mtpstudio/propsofdoraemon/entity/EntityDx.java
@@ -0,0 +1,43 @@
+package com.joy187.re8joymod.common.entity;
+
+import net.minecraft.entity.AgeableEntity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.ai.attributes.AttributeModifierMap;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.monster.RavagerEntity;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.PigEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Items;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+
+public class EntityDx extends AnimalEntity {
+    public EntityDx(EntityType<? extends AnimalEntity> type, World worldIn) {
+        super(type, worldIn);
+        this.xpReward = 25;
+    }
+
+    public static AttributeModifierMap.MutableAttribute setAttributes() {
+        return MobEntity.createMobAttributes().add(Attributes.MAX_HEALTH, 20.0D).
+                add(Attributes.MOVEMENT_SPEED, 0.25D).
+                add(Attributes.ATTACK_DAMAGE, 5.0D);
+    }
+
+    protected void registerGoals() {
+        this.goalSelector.addGoal(0, new SwimGoal(this));
+        this.goalSelector.addGoal(1, new PanicGoal(this, 1.25D));
+        this.goalSelector.addGoal(5, new FollowParentGoal(this, 1.1D));
+        this.goalSelector.addGoal(6, new WaterAvoidingRandomWalkingGoal(this, 1.0D));
+        this.goalSelector.addGoal(7, new LookAtGoal(this, PlayerEntity.class, 6.0F));
+        this.goalSelector.addGoal(8, new LookRandomlyGoal(this));
+    }
+
+    @Override
+    public AgeableEntity getBreedOffspring(ServerWorld p_241840_1_, AgeableEntity p_241840_2_) {
+        return null;
+    }
+}
---
+++ src/main/java/cn/mtpstudio/propsofdoraemon/entity/EntityJx.java
@@ -0,0 +1,40 @@
+package com.joy187.re8joymod.common.entity;
+
+import net.minecraft.entity.AgeableEntity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.ai.attributes.AttributeModifierMap;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+
+public class EntityJx extends EntityDx{
+    public EntityJx(EntityType<? extends AnimalEntity> type, World worldIn) {
+        super(type, worldIn);
+        this.xpReward = 25;
+    }
+
+    public static AttributeModifierMap.MutableAttribute setAttributes() {
+        return MobEntity.createMobAttributes().add(Attributes.MAX_HEALTH, 20.0D).
+                add(Attributes.MOVEMENT_SPEED, 0.27D).
+                add(Attributes.ATTACK_DAMAGE, 4.5D);
+    }
+
+    protected void registerGoals() {
+        this.goalSelector.addGoal(0, new SwimGoal(this));
+        this.goalSelector.addGoal(1, new PanicGoal(this, 1.25D));
+        this.goalSelector.addGoal(5, new FollowParentGoal(this, 1.1D));
+        this.goalSelector.addGoal(6, new WaterAvoidingRandomWalkingGoal(this, 1.0D));
+        this.goalSelector.addGoal(7, new LookAtGoal(this, PlayerEntity.class, 6.0F));
+        this.goalSelector.addGoal(8, new LookRandomlyGoal(this));
+    }
+
+    @Override
+    public AgeableEntity getBreedOffspring(ServerWorld p_241840_1_, AgeableEntity p_241840_2_) {
+        return null;
+    }
+
+}
